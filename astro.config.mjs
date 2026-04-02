2ace2080416e25dd795d151a5184bb713deca51f
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2026-04-02 17:45:30 +0700

    Temporary use github pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://buruhcoding.id',
+  site: 'https://buruh-coding.github.io',
+  base: '/website',
   integrations: [sitemap()]
 });
